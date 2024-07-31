96124d75264fdbf12bad9b0b2bbc7e00288de372
Nat Williams <nat.williams@gmail.com>
2024-07-31 10:54:42 -0500

    first stab at my hillside keymap

--- keyboards/hillside/46/keymaps/natw/config.h
+++ keyboards/hillside/46/keymaps/natw/config.h
@@ -7,3 +7,5 @@
 // Such as Elite-C v3, but not v3.1 on. Also apparently some ProMicro boards.
 // For those boands, use usb detection instead.
 // #define SPLIT_USB_DETECT
+
+#define PERMISSIVE_HOLD
