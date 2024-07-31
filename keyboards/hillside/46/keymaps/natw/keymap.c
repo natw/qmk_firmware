8986272e05d6ba8a5ddada90cdf73cd52997afc2
Nat Williams <nat.williams@gmail.com>
2024-07-31 11:35:03 -0500

    more?

--- keyboards/hillside/46/keymaps/natw/keymap.c
+++ keyboards/hillside/46/keymaps/natw/keymap.c
@@ -26,17 +26,15 @@ enum layers {
 #define KC_RPAREN S(KC_0)
 
 #define KC_HASH S(KC_3)
-#define KC_DOLLAR S(KC_4)
-#define KC_PERCENT S(KC_5)
 #define KC_CARET S(KC_6)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_EQUAL,
-     NW_CTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,                   CW_TOGG,  KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                          KC_LOPT, KC_SPC,  KC_LGUI, LY_SYM,  LY_NAV, KC_ENTER, KC_LALT, KC_MINUS
+     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                           KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_EQUAL,
+     NW_CTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                           KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,                                      CW_TOGG,  KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                          KC_LOPT, KC_SPC,  KC_LGUI, LY_SYM,                     LY_NAV, KC_ENTER, KC_SPC, KC_MINUS
     ),
     /*[_NAV] = LAYOUT(*/
     /* COPY   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,*/
@@ -45,16 +43,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*                                     _______, _______, _______, _______,    LY_ADJ , _______, _______, _______*/
     /*),*/
     [_SYM_NUM] = LAYOUT(
-     _______, KC_EXLM,    KC_AT,     KC_LCURL,  KC_RCURL,  KC_PIPE,                                   KC_CIRC, KC_7, KC_8, KC_9, KC_RPRN, KC_DEL,
-     _______, KC_HASH,    KC_DOLLAR, KC_LPAREN, KC_RPAREN, KC_GRAVE,                                  KC_HOME, KC_4, KC_5, KC_6, KC_PPLS, KC_END,
-     _______, KC_PERCENT, KC_CARET,  KC_LBRC,   KC_RBRC,   _______,  ALT_GR,                 KC_APP,  KC_MUTE, KC_1, KC_2, KC_3, _______, KC_RCTL,
-                                     _______,   _______,   _______,  _______,  KC_BACKSLASH, _______, _______, KC_0 
+     _______, KC_EXLM,    KC_AT,     KC_LCURL,  KC_RCURL,  KC_PIPE,                                                KC_CIRC, KC_7, KC_8, KC_9, KC_RPRN, KC_DEL,
+     _______, KC_HASH,    KC_DOLLAR, KC_LPAREN, KC_RPAREN, KC_GRAVE,                                               KC_HOME, KC_4, KC_5, KC_6, KC_PPLS, KC_END,
+     _______, KC_PERCENT, KC_CARET,  KC_LBRC,   KC_RBRC,   _______,  _______,                             _______, KC_AMPR, KC_1, KC_2, KC_3, _______, KC_RCTL,
+                                                _______,   _______,  _______, _______,      KC_BACKSLASH, _______, _______, KC_0 
     ),
     [_NAV] = LAYOUT(
-     _______, _______, _______, _______, _______, _______,                                          KC_PGUP, _______, _______, _______,  KC_BRIU, KC_VOLU, 
-     _______, _______, _______, _______, _______, _______,                                          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_BRID, KC_VOLD, 
-     _______, _______, _______, _______, _______, _______, _______,                        _______, KC_PGUP, KC_MPRV, PLY_PAU, KC_MNXT,  _______, _______,
-                                         _______, _______, _______, KC_BACKSLASH, _______, _______, _______, _______
+     KC_ESC,  KC_EXLM,    KC_AT,     KC_LCURL,  KC_RCURL,  KC_PIPE,                                                     KC_PGUP, _______, _______, _______,  KC_BRIU, KC_VOLU, 
+     _______, KC_HASH,    KC_DOLLAR, KC_LPAREN, KC_RPAREN, KC_GRAVE,                                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_BRID, KC_VOLD, 
+     _______, KC_PERCENT, KC_CARET,  KC_LBRC,   KC_RBRC,   _______,  _______,                                   _______, KC_PGUP, KC_MPRV, PLY_PAU, KC_MNXT,  _______, _______,
+                                                _______,   _______,  _______, KC_BACKSLASH,            _______, _______, _______, _______
     ),
     /*[_ADJUST] = LAYOUT(*/
     /* xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, AG_SWAP, CG_SWAP,                                        xxxxxxx, KC_F11 , KC_F12 , KC_PSCR, xxxxxxx, EE_CLR,*/
