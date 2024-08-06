74252f77a3d808222337832c23d9539846128b67
Nat Williams <nat.williams@gmail.com>
2024-08-06 14:48:05 -0500

    both thumbs for numpad layer

--- keyboards/hillside/46/keymaps/natw/keymap.c
+++ keyboards/hillside/46/keymaps/natw/keymap.c
@@ -15,7 +15,9 @@ enum layers {
 #define PLY_PAU  KC_MEDIA_PLAY_PAUSE
 
 #define NW_CTL MT(MOD_RCTL, KC_BSPC)
-#define NW_LSFT MT(MOD_LSFT, KC_TILDE)
+#define NW_LSFT MT(MOD_LSFT, S(KC_GRAVE))
+#define NW_SPC LT(_NAV, KC_SPC)
+
 #define KC_LCURL S(KC_LBRC)
 #define KC_RCURL S(KC_RBRC)
 #define KC_LPAREN S(KC_9)
@@ -24,20 +26,21 @@ enum layers {
 #define KC_HASH S(KC_3)
 #define KC_CARET S(KC_6)
 
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                         KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_EQUAL,
      NW_CTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                         KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      NW_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,                                    CW_TOGG,  KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                          KC_LOPT, KC_SPC,  KC_LGUI, LY_SYM,                   LY_NAV, KC_ENTER, KC_SPC, KC_MINUS
+                                          KC_LOPT, KC_SPC,  KC_LGUI, LY_SYM,                   LY_SYM, KC_ENTER, NW_SPC, KC_MINUS
     ),
 
     [_SYM_NUM] = LAYOUT(
-     KC_ESC,  KC_EXLM,    KC_AT,     KC_LCURL,  KC_RCURL,  KC_PIPE,                                                   KC_CIRC, KC_7, KC_8, KC_9, KC_PAST, KC_DEL,
-     _______, KC_HASH,    KC_DOLLAR, KC_LPAREN, KC_RPAREN, KC_GRAVE,                                                  KC_HOME, KC_4, KC_5, KC_6, KC_PPLS, KC_END,
-     _______, KC_PERCENT, KC_CARET,  KC_LBRC,   KC_RBRC,   _______,  _______,                                _______, KC_AMPR, KC_1, KC_2, KC_3, KC_DOT,  KC_RCTL,
-                                                _______,   _______,  _______, _______,         KC_BACKSLASH, _______, _______, KC_0 
+     KC_ESC,   KC_EXLM,    KC_AT,     KC_LCURL,  KC_RCURL,  KC_PIPE,                                                   KC_CIRC, KC_7, KC_8, KC_9, KC_PAST, KC_DEL,
+     _______,  KC_HASH,    KC_DOLLAR, KC_LPAREN, KC_RPAREN, KC_GRAVE,                                                  KC_HOME, KC_4, KC_5, KC_6, KC_PPLS, KC_END,
+     KC_TILDE, KC_PERCENT, KC_CARET,  KC_LBRC,   KC_RBRC,   _______,  _______,                                _______, KC_AMPR, KC_1, KC_2, KC_3, KC_DOT,  KC_RCTL,
+                                                 _______,   _______,  _______, KC_BACKSLASH,    KC_BACKSLASH, _______, _______, KC_0 
     ),
 
     [_NAV] = LAYOUT(
