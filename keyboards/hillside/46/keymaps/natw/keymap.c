5fdcf8ab34ffa350ec80423d8acad14a5f49fb87
Nat Williams <nat.williams@gmail.com>
2024-08-06 15:26:41 -0500

    normal left space

--- keyboards/hillside/46/keymaps/natw/keymap.c
+++ keyboards/hillside/46/keymaps/natw/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                         KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_EQUAL,
      NW_CTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                         KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      NW_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LY_NAV,                                    CW_TOGG,  KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                          KC_LOPT, NW_SPC,  KC_LGUI, LY_SYM,                   LY_SYM, KC_ENTER, NW_SPC, KC_MINUS
+                                          KC_LOPT, KC_SPC,  KC_LGUI, LY_SYM,                   LY_SYM, KC_ENTER, NW_SPC, KC_MINUS
     ),
 
     [_SYM_NUM] = LAYOUT(
